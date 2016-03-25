f2a96e660e1636630f4a061337a1f5c33eb1892a
Rodolfo <Rodolfo@123.local>
2016-03-25 18:24:05 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by Rodolfo on 16/3/25.
+//  Copyright © 2016年 贝沃汇力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by Rodolfo on 16/3/25.
+//  Copyright © 2016年 贝沃汇力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
